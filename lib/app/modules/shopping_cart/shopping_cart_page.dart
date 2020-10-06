ca94804fc315f1d8cacef3f3519b7e801f3d3b61
Matheus Figueirêdo <matheustgf64@gmail.com>
2020-10-06 16:58:38 -0300

    The first style of Shopping Cart was developed

--- lib/app/modules/shopping_cart/shopping_cart_page.dart
+++ lib/app/modules/shopping_cart/shopping_cart_page.dart
@@ -1,11 +1,12 @@
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:osiris/app/shared/pages/bottom_bar/bottom_bar_page.dart';
 import 'shopping_cart_controller.dart';
 
 class ShoppingCartPage extends StatefulWidget {
   final String title;
-  const ShoppingCartPage({Key key, this.title = "ShoppingCart"})
-      : super(key: key);
+  const ShoppingCartPage({Key key, this.title = "Carrinho"}) : super(key: key);
 
   @override
   _ShoppingCartPageState createState() => _ShoppingCartPageState();
@@ -21,8 +22,62 @@ class _ShoppingCartPageState
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Column(
-        children: <Widget>[],
+      body: Container(
+        child: Column(
+          children: [
+            ListView.builder(
+                itemCount: 3,
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: <Widget>[
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        child: Row(
+                          children: [
+                            Container(
+                              child: Image.network(
+                                'https://i.picsum.photos/id/1080/6858/4574.jpg?hmac=qMYBjROs2Wu589QQXRAYsxDJu4ZuRQ4PKDpb3x_Oouw',
+                                width: 150,
+                                height: 100,
+                              ),
+                            ),
+                            SizedBox(width: 20),
+                            Column(
+                              children: [
+                                Text('Morangos ' + (index + 1).toString()),
+                                Text('R\$ 19,99'),
+                              ],
+                            ),
+                            Spacer(),
+                            MaterialButton(
+                                onPressed: () {
+                                  print('Comprado!');
+                                },
+                                child: Text('Comprar')),
+                          ],
+                        ),
+                      ),
+                    ],
+                  );
+                }),
+            SizedBox(height: 10),
+            Row(
+              children: [
+                Spacer(),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Finalizar Compra'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomBarPage(
+        bottomBarCurrentIndex: 2,
       ),
     );
   }
